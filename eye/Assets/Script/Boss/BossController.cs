fe2220a984b4a0262fc6d16b251a05d6cc303ec2
ssy05 <87408309+ssy05@users.noreply.github.com>
2022-12-20 19:59:06 +0900

    Boss
    Boss

--- eye/Assets/Script/Boss/BossController.cs
+++ eye/Assets/Script/Boss/BossController.cs
@@ -6,16 +6,33 @@ public class BossController : MonoBehaviour
 {
     public GameObject[] BossHitBox;
     public GameObject[] BossAttackBox;
+    public GameObject BossSkill;
+    public GameObject[] BossSkillspawn;
+
+    public Animator anime;
+    int ani = 0;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        anime = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            ani++;
+            Debug.Log(ani);
+            anime.SetInteger("count", ani);
+        }
+        if (ani >= 4)
+        {
+            ani = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+            anime.SetInteger("count", 3);
     }
 
     public void ShowBossHitBox(int hit)
@@ -31,4 +48,11 @@ public class BossController : MonoBehaviour
     {
         BossAttackBox[attack].SetActive(false);
     }
+
+    public void LeftBossSkillProduce()
+    {
+        for(int i =  0;i < 3;i++)
+            Instantiate(BossSkill, new Vector3(-2 + i, 0.6f, 0), Quaternion.identity);
+    }
+
 }
